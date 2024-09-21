d4ca13ddc831495e9c7e0a2c273ebad1540facbe
Jliu-1 <jliu217@central.uh.edu>
2024-09-20 22:19:06 -0500

    recov geb r

--- include/SZ3/api/impl/SZInterp.hpp
+++ include/SZ3/api/impl/SZInterp.hpp
@@ -72,7 +72,7 @@ char *SZ_compress_Interp(SZ::Config &conf, T *data, size_t &outSize) {
             double best_ratio = current_ratio;
             // check smaller bounds
             
-            while(conf.qoi ==1  or conf.qoi == 3){//was true
+            while(1){
                 auto prev_eb = conf.absErrorBound;
                 prev_ratio = current_ratio;
                 conf.absErrorBound /= 2;
