161f9a137d1d7d790a450839a61387678ada5d00
Jliu-1 <jliu217@central.uh.edu>
2024-09-22 11:49:37 -0500

    sym

--- include/SZ3/qoi/FX.hpp
+++ include/SZ3/qoi/FX.hpp
@@ -43,7 +43,7 @@ namespace SZ {
             //ddf = diff(df,x);
             ddf = df.diff(x.rcp_from_this());
 
-            std::cout<<(double)evalf(df.subs(map_basic_basic({{x:2}})),53, SymEngine::EvalfDomain::Real)<<std::endl;
+            std::cout<<(double)evalf(df.subs(map_basic_basic({{x:Symbol(2)}})),53, SymEngine::EvalfDomain::Real)<<std::endl;
         }
 
         using Range = multi_dimensional_range<T, N>;
