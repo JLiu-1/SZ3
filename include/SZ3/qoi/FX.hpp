00c9e5abc1f3d4ef0e19c1a62fae4bb8544a5e86
Jliu-1 <jliu217@central.uh.edu>
2024-09-22 10:47:24 -0500

    sym

--- include/SZ3/qoi/FX.hpp
+++ include/SZ3/qoi/FX.hpp
@@ -33,7 +33,7 @@ namespace SZ {
             // TODO: adjust type for int data
             //printf("global_eb = %.4f\n", (double) global_eb);
             concepts::QoIInterface<T, N>::id = 14;
-            SymEngine::RCP<const Symbol>  x = const Symbol("x");
+            SymEngine::RCP<const Symbol>  x = Symbol("x");
     
             f = parse(ff);
             //df = diff(f,x);
