9790cb723922f033e961eae12a12983f3337db4f
Jliu-1 <jliu217@central.uh.edu>
2024-09-22 12:52:24 -0500

    sym

--- include/SZ3/qoi/FX.hpp
+++ include/SZ3/qoi/FX.hpp
@@ -52,8 +52,8 @@ namespace SZ {
             std::cout<<"df: "<< df<<std::endl;
             std::cout<<"ddf: "<< ddf<<std::endl;
            // RCP<const Basic> result = evalf(df.subs(map_basic_basic({{x,RealDouble(2).rcp_from_this()}})),53, SymEngine::EvalfDomain::Real);
-            RealDouble value(2);
-            map_basic_basic mbb=  {{x,value.rcp_from_this()}};
+            RCP<RealDouble> value = RCP<RealDouble>(new RealDouble(2));
+            map_basic_basic mbb=  {{x,value}};
             std::cout<<"init 5 "<< std::endl;
              double result = (double)df.subs( mbb); 
            
