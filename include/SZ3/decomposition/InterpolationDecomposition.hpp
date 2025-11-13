1b39daea9c1c5c1c3361b5ccc6cba92523534f37
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 02:30:55 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -274,7 +274,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
         quantizer.postcompress_data();
         delete []buffer;
         delete []aligned_buffer;
-        std::cout<<quant_inds_vec.size()<<std::endl;
+        //std::cout<<quant_inds_vec.size()<<std::endl;
         return quant_inds_vec;
     }
 
