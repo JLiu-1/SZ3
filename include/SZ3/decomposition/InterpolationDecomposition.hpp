0088da7da6b658ce9d9312b66cbe2b198ee9fba5
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 12:30:11 -0600

    z avx

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -1048,12 +1048,8 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                 begin_idx[dims[i]] = begin[dims[i]];
                 begin_idx[dims[i - 1]] = (begin[dims[i - 1]] ? begin[dims[i - 1]] + stride : 0);
                 strides[dims[i - 1]] = stride;
-                if(N==3 &&stride <=2 && dims[i] == 0)
-                    predict_error += interpolation_1d_simd_3d_x(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
-                else if(N==3 &&stride <=2 && dims[i] == 1)
-                    predict_error += interpolation_1d_simd_3d_y(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
-                else
-                    predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
+               
+                predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
                 }
             }
 
