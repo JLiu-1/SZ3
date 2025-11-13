03e3bd22d3ce963932d1ffd7a6716bea26f9d22b
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 12:28:17 -0600

    z avx

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -939,8 +939,8 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                     });
             }
         } else {
-            size_t stride3x = 3 * stride;
-            size_t i_start = 3;
+            //size_t stride3x = 3 * stride;
+            //size_t i_start = 3;
             begins[direction] = 1;
             ends[direction] = n;
             strides[direction] = 2;
@@ -1027,10 +1027,10 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                     begin_idx[2] = begin[2];
                     begin_idx[1] = (begin[1] ? begin[1] + stride : 1);
                     strides[1] = stride;
-                    predict_error += interpolation_1d_simd_3d_z(data, begin_idx, end_idx, dims[2], strides, stride, interp_func, quantize_func);
+                    predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[2], strides, stride, interp_func, quantize_func);
                 }
                 else{//zyx
-                    predict_error += interpolation_1d_simd_3d_z(data, begin_idx, end_idx, dims[0], strides, stride, interp_func, quantize_func);
+                    predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[0], strides, stride, interp_func, quantize_func);
                     begin_idx[1] = begin[1];
                     begin_idx[2] = (begin[2] ? begin[2] + stride : 0);
                     strides[2] = stride;
