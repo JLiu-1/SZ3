7f6de0b21c271a47684ca9cf948857a272309319
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 05:21:42 -0600

    avx

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -848,9 +848,9 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                 begin_idx[dims[i]] = (begin[dims[i]] ? begin[dims[i]] + stride2x : 0);
                 strides[dims[i]] = stride2x;
             }
-            if(N==3 &&stride == 1 && dims[0] ==0)
+            if(N==3 &&stride<=2  && dims[0] ==0)
                 predict_error += interpolation_1d_simd_3d_x(data, begin_idx, end_idx, dims[0], strides, stride, interp_func, quantize_func);
-            else if(N==3 &&stride == 1 && dims[0] ==0)
+            else if(N==3 &&stride <=2  && dims[0] ==0)
                 predict_error += interpolation_1d_simd_3d_y(data, begin_idx, end_idx, dims[0], strides, stride, interp_func, quantize_func);
             else
                 predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[0], strides, stride, interp_func, quantize_func);
@@ -859,9 +859,9 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                 begin_idx[dims[i]] = begin[dims[i]];
                 begin_idx[dims[i - 1]] = (begin[dims[i - 1]] ? begin[dims[i - 1]] + stride : 0);
                 strides[dims[i - 1]] = stride;
-                if(N==3 &&stride == 1 && dims[i] == 0)
+                if(N==3 &&stride <=2 && dims[i] == 0)
                     predict_error += interpolation_1d_simd_3d_x(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
-                else if(N==3 &&stride == 1 && dims[i] == 1)
+                else if(N==3 &&stride <=2 && dims[i] == 1)
                     predict_error += interpolation_1d_simd_3d_y(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
                 else
                     predict_error += interpolation_1d_fastest_dim_first(data, begin_idx, end_idx, dims[i], strides, stride, interp_func, quantize_func);
