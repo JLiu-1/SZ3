0c7fd1459d6d291fff4da8d1d0006a43c7643155
Jliu-1 <jliu217@central.uh.edu>
2025-11-12 20:26:44 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -257,9 +257,9 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                                 size_t idx = x * offset_x + y * offset_y + z;
                                 T fix = ori_data[idx]-data[idx];
                                 int fix_q =(int)(fix/q_unit);
-                                if(fix_q > 1000000){
-                                    std::cout<<x<<" "<<y<<" "<<z<<" "<<ori_data[idx]<<" "<<data[idx]<<std::endl;
-                                }
+                                //if(fix_q > 1000000){
+                                //    std::cout<<x<<" "<<y<<" "<<z<<" "<<ori_data[idx]<<" "<<data[idx]<<std::endl;
+                                //}
                                 data[idx]+=q_unit*fix_q;
 
                                 quant_inds_vec_post.push_back(fix_q + q_center);
@@ -406,16 +406,16 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                     }
                 }
             }
-            std::cout<<*std::max_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
-         std::cout<<*std::min_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
+        //    std::cout<<*std::max_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
+      //  std::cout<<*std::min_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
             //std::cout<<quant_inds_vec_post.size()<<std::endl;
 
 
         }
         //std::cout<<quant_inds_vec.size()<<std::endl;
         //std::cout<<quant_inds_vec_post.size()<<std::endl;
-        std::cout<<*std::max_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
-         std::cout<<*std::min_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
+       // std::cout<<*std::max_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
+       //  std::cout<<*std::min_element(quant_inds_vec_post.begin(), quant_inds_vec_post.end())<<std::endl;
         quant_inds_vec.reserve(quant_inds_vec.size() + quant_inds_vec_post.size());
         quant_inds_vec.insert(quant_inds_vec.end(),quant_inds_vec_post.begin(), quant_inds_vec_post.end());
 
