58d93392cc0208497d0a8b781e4c2e7afb94e71a
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 04:00:17 -0600

    avx

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -544,8 +544,8 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                         for (size_t k = begins[2]; k < ends[2]; k += strides[2]) {
                             auto cur_offset =  cur_ij_offset + k;
                             cur_buffer_1[buffer_idx] = data[cur_offset -  stride3x];
-                            cur_buffer_2[buffer_idx] = data[cur_offset - stridex];
-                            cur_buffer_3[buffer_idx] = data[cur_offset + stridex];
+                            cur_buffer_2[buffer_idx] = data[cur_offset - stride];
+                            cur_buffer_3[buffer_idx] = data[cur_offset + stride];
                             cur_buffer_4[buffer_idx] = data[cur_offset +  stride3x];
                             buffer_idx++;
 
