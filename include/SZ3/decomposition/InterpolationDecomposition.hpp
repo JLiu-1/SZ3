49927dbc95799fde13703a3d839239acd4914418
Jliu-1 <jliu217@central.uh.edu>
2025-11-13 00:06:08 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -273,7 +273,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
             }
         }
         if(x %32 ==0 &&y %32 ==0 &&z %32 ==0)
-            std::cout<<x<<" "<<y<<" "<<z<<" "<<speck_x<<" "<<speck_y<<speck_z<<std::endl;
+            std::cout<<x<<" "<<y<<" "<<z<<" "<<speck_x<<" "<<speck_y<<" "<<Wspeck_z<<std::endl;
         return speck_x * original_dim_offsets[0] + speck_y * original_dim_offsets[1] + speck_z;
 
     }
