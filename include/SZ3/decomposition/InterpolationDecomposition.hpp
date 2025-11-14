02ff46e47881c2ce81374c7e670efb45e2380ba3
Jliu-1 <jliu217@central.uh.edu>
2025-11-14 16:45:27 -0600

    post fix

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -407,18 +407,18 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                                             std::cout<<x<<" "<<y<<" "<<z<<" "<<offset+z<<std::endl;
                                             __m256 v_x = _mm256_loadu_ps(cur_pos + z);
                                             __m256 v_y = _mm256_loadu_ps(cur_pos_ori + z);
-                                            std::cout<<"3.1"<<std::end;
+                                            std::cout<<"3.1"<<std::endl;
                                             v_x = _mm256_mul_ps(v_x,v_a);
                                             v_x = _mm256_add_ps(v_x, v_b);
                                             v_y = _mm256_sub_ps(v_y, v_x);
                                             v_y = _mm256_andnot_ps(mask,v_y);
                                             v_max_abs_err = _mm256_max_ps(v_max_abs_err,v_y);
-                                            std::cout<<"3.2"<<std::end;
+                                            std::cout<<"3.2"<<std::endl;
 
 
                                         }
                                         for (; z < block_size; ++z){
-                                            std::cout<<"3.3"<<std::end;
+                                            std::cout<<"3.3"<<std::endl;
                                             max_abs_err_post_correction = std::max(max_abs_err_post_correction, std::abs(cur_pos_ori[z] - a * cur_pos[z] - b) );
                                         }
         
