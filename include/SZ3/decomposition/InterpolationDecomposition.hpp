192acd0be94edb92bb2909bfa18bdd32487d44f7
Jliu-1 <jliu217@central.uh.edu>
2025-11-14 17:08:06 -0600

    post fix

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -282,7 +282,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
 
             //size_t stride2x = stride * 2;
             double q_unit_a = 0.05;
-            double q_unit_b = 0.05 * eb;
+            double q_unit_b = 0.025 * eb;
 
             //int raw_block_size = 8;//or 8 * stride
             //int block_size = raw_block_size - raw_block_size % stride;
