dc65aa5bdfd0ca462949376a655df3c0d6a44aae
Jliu-1 <jliu217@central.uh.edu>
2025-11-15 14:13:01 -0600

    range debug

--- include/SZ3/utils/Statistic.hpp
+++ include/SZ3/utils/Statistic.hpp
@@ -21,6 +21,8 @@ T data_range(const T *data, size_t num) {
             if (max < data[i]) max = data[i];
             if (min > data[i]) min = data[i];
         }
+        std::cout<<max<<std::endl;
+        std::cout<<min<<std::endl;
         return max - min;
 
     }
@@ -54,7 +56,8 @@ T data_range(const T *data, size_t num) {
             maxval = std::max(maxval, data[i]);
             minval = std::max(minval, data[i]);
         }
-
+        std::cout<<maxval<<std::endl;
+        std::cout<<minval<<std::endl;
         return maxval - minval;
     }
     else if constexpr (is_double){
