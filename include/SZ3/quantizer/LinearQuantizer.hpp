e95ee2f8b6b9704e5fd6fc9c76cbaf9e7940af1c
Jliu-1 <jliu217@central.uh.edu>
2025-11-15 14:19:57 -0600

    quant revert

--- include/SZ3/quantizer/LinearQuantizer.hpp
+++ include/SZ3/quantizer/LinearQuantizer.hpp
@@ -11,38 +11,53 @@
 #include "SZ3/utils/MemoryUtil.hpp"
 
 namespace SZ3 {
-
 template <class T>
 class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
-   public:
-    LinearQuantizer() : error_bound(1), double_error_bound(2), double_error_bound_reciprocal(0.5), radius(32768) {}
+public:
+    LinearQuantizer()
+        : error_bound(1),
+          error_bound_reciprocal(1),
+          radius(32768) {
+    }
 
-    LinearQuantizer(double eb, int r = 32768) : error_bound(eb),double_error_bound(2*eb), double_error_bound_reciprocal(0.5 / eb), radius(r) {
+    LinearQuantizer(double eb, int r = 32768, bool _strict_eb = true)
+        : error_bound(eb),
+          error_bound_reciprocal(1.0 / eb),
+          radius(r),
+          strict_eb(_strict_eb) {
         assert(eb != 0);
     }
+
 
     double get_eb() const { return error_bound; }
 
     void set_eb(double eb) {
         error_bound = eb;
-        double_error_bound = 2 * eb;
-        double_error_bound_reciprocal = 1.0 / double_error_bound;
+        error_bound_reciprocal = 1.0 / eb;
     }
 
     std::pair<int, int> get_out_range() const override { return std::make_pair(0, radius * 2); }
 
     // quantize the data with a prediction value, and returns the quantization index and the decompressed data
     // int quantize(T data, T pred, T& dec_data);
-    ALWAYS_INLINE int quantize_and_overwrite(T &data, T pred) override {
+    ALWAYS_INLINE int quantize_and_overwrite(T& data, T pred) override {
         T diff = data - pred;
-        int quant_index = std::llrint(std::abs(diff) * this->double_error_bound_reciprocal);
-        if (quant_index < this->radius ) {
-            if (diff < 0) 
+        auto quant_index = static_cast<int64_t>(fabs(diff) * this->error_bound_reciprocal) + 1;
+        if (quant_index < this->radius * 2) {
+            quant_index >>= 1;
+            int half_index = quant_index;
+            quant_index <<= 1;
+            int quant_index_shifted;
+            if (diff < 0) {
                 quant_index = -quant_index;
-            auto quant_index_shifted = this->radius + quant_index;
-            T decompressed_data = pred + quant_index * this->double_error_bound;
-            // if data is NaN, the error is NaN, and NaN <= error_bound is false
-            if (fabs(decompressed_data - data) <= this->error_bound) {
+                quant_index_shifted = this->radius - half_index;
+            } else {
+                quant_index_shifted = this->radius + half_index;
+            }
+            T decompressed_data = pred + quant_index * this->error_bound;
+            // if data is NaN, the diff is NaN, and NaN <= 0 is false
+            diff = fabs(decompressed_data - data);
+            if (diff <= this->error_bound || (!strict_eb && diff <= this->error_bound * 1.1)) {
                 data = decompressed_data;
                 return quant_index_shifted;
             } else {
@@ -65,7 +80,7 @@ class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
     }
 
     ALWAYS_INLINE T recover_pred(T pred, int quant_index) {
-        return pred + (quant_index - this->radius) * this->double_error_bound;
+        return pred + 2 * (quant_index - this->radius) * this->error_bound;
     }
 
     ALWAYS_INLINE T recover_unpred() { return unpred[index++]; }
@@ -77,7 +92,7 @@ class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
 
     size_t size_est() { return unpred.size() * sizeof(T); }
 
-    void save(unsigned char *&c) const override {
+    void save(unsigned char*& c) const override {
         write(uid, c);
         write(this->error_bound, c);
         write(this->radius, c);
@@ -88,15 +103,14 @@ class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
         }
     }
 
-    void load(const unsigned char *&c, size_t &remaining_length) override {
+    void load(const unsigned char*& c, size_t& remaining_length) override {
         uchar uid_read;
         read(uid_read, c, remaining_length);
         if (uid_read != uid) {
             throw std::invalid_argument("LinearQuantizer uid mismatch");
         }
-        double eb;
-        read(eb, c, remaining_length);
-        set_eb(eb);
+        read(this->error_bound, c, remaining_length);
+        this->error_bound_reciprocal = 1.0 / this->error_bound;
         read(this->radius, c, remaining_length);
         size_t unpred_size = 0;
         read(unpred_size, c, remaining_length);
@@ -111,16 +125,15 @@ class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
         printf("[LinearQuantizer] error_bound = %.8G, radius = %d, unpred = %zu\n", error_bound, radius, unpred.size());
     }
 
-   private:
+private:
     std::vector<T> unpred;
-    size_t index = 0;  // used in decompression only
+    size_t index = 0; // used in decompression only
     uchar uid = 0b10;
 
     double error_bound;
-    double double_error_bound;
-    double double_error_bound_reciprocal;
-    int radius;  // quantization interval radius
+    double error_bound_reciprocal;
+    int radius; // quantization interval radius
+    bool strict_eb = true;
 };
-
-}  // namespace SZ3
+} // namespace SZ3
 #endif
