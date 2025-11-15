f711b0cd9cd720deede031f0b9dc8121ab14e693
Jliu-1 <jliu217@central.uh.edu>
2025-11-15 12:46:49 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -818,13 +818,14 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                         }
                         if(a_q!=0 || b_q!=0)
                             fixed_block_count ++;
-                        if (block_idx % 5000 == 0)
-                             std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<" "<<quant_index-1<<" "<<num_blocks<<std::endl;               
+                        
                         
                         quant_inds [quant_index] = a_q + quant_center;
                         quant_inds [quant_index + num_blocks] = b_q + quant_center;
                         quant_index++;
                         block_idx++;
+                        if (block_idx % 5000 == 0)
+                             std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<" "<<quant_index-1<<" "<<num_blocks<<std::endl;               
 
 
                     }
