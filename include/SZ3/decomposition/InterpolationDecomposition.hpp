fda85ac912c1a9091573f3e97675c7053d084624
Jliu-1 <jliu217@central.uh.edu>
2025-11-15 12:44:31 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -110,8 +110,8 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                         int a_q = quant_inds[quant_index] - quant_center;
                         int b_q = quant_inds[quant_index +num_blocks] - quant_center;
                         quant_index++;
-                         if (block_idx % 5000 == 0)
-                            std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<std::endl;
+                        if (block_idx % 5000 == 0)
+                            std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<" "<<quant_index-1<<" "<<num_blocks<<std::endl;
                         block_idx++;
 
                         if(a_q !=0 || b_q!= 0){
@@ -819,7 +819,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
                         if(a_q!=0 || b_q!=0)
                             fixed_block_count ++;
                         if (block_idx % 5000 == 0)
-                            std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<std::endl;                        
+                             std::cout<<block_idx<<" "<<a_q<<" "<<b_q<<" "<<quant_index-1<<" "<<num_blocks<<std::endl;               
                         
                         quant_inds [quant_index] = a_q + quant_center;
                         quant_inds [quant_index + num_blocks] = b_q + quant_center;
