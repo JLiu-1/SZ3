ad40bf499caf11740cb254b114363fefbbdc9340
Jliu-1 <jliu217@central.uh.edu>
2025-11-15 15:00:42 -0600

    avx iter check

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -531,7 +531,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
         size_t i = 0;
 
         if constexpr (is_float) {
-            const size_t step =3;
+            const size_t step = 4;
             const __m256 nine  = _mm256_set1_ps(9.0f);
             const __m256 factor = _mm256_set1_ps(1.0f / 16.0f);
 
