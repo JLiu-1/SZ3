4b1a244868281ba117f9156f4b9f24dd00ae5ba2
Jliu-1 <jliu217@central.uh.edu>
2025-11-14 23:41:25 -0600

    post fix

--- include/SZ3/compressor/SZGenericCompressor.hpp
+++ include/SZ3/compressor/SZGenericCompressor.hpp
@@ -38,6 +38,11 @@ class SZGenericCompressor : public concepts::CompressorInterface<T> {
     size_t compress(const Config &conf, T *data, uchar *cmpData, size_t cmpCap) override {
         //Timer timer(true);  
         std::vector<int> quant_inds = decomposition.compress(conf, data);
+        std::vector<int> suffix_quant_inds;
+        if(quant_inds_size > conf.num){
+            suffix_quant_inds.insert(suffix_quant_inds.end(),quant_inds.begin()+conf.num,quant_inds.end);
+            quant_inds.resize(conf.num;)
+        }
         //timer.stop("interpquant");
         //timer.start();
         if (decomposition.get_out_range().first != 0) {
@@ -47,10 +52,15 @@ class SZGenericCompressor : public concepts::CompressorInterface<T> {
         //timer.stop("prepro_huff");
         //timer.start();
         size_t bufferSize = std::max<size_t>(
-            1000, 1.2 * (decomposition.size_est() + encoder.size_est() + sizeof(T) * quant_inds.size()));
+            1000, 1.2 * (decomposition.size_est() + encoder.size_est() + sizeof(T) * quant_inds.size()+ sizeof(T) * suffix_inds.size()));
 
         auto buffer = static_cast<uchar *>(malloc(bufferSize));
         uchar *buffer_pos = buffer;
+        if(suffix_quant_inds > conf.num)
+            *buffer_pos = 1;
+        else
+            *buffer_pos = 0;
+        buffer_pos++;
 
         decomposition.save(buffer_pos);
         encoder.save(buffer_pos);
@@ -61,12 +71,30 @@ class SZGenericCompressor : public concepts::CompressorInterface<T> {
         //timer.start();
         encoder.encode(quant_inds, buffer_pos);
         encoder.postprocess_encode();
+        
+
+        size_t huff_size = buffer_pos - buffer;
+        if(suffix_quant_inds > conf.num){
+            auto old_pos = buffer_pos;
+            encoder.preprocess_encode(suffix_quant_inds, decomposition.get_out_range().second);
+            write<size_t>(quant_inds_suffix.size(), buffer_pos);
+            
+            encoder.encode(quant_inds_suffix, buffer_pos);
+            auto suffix_huff_size = buffer_pos - old_pos;
+            if (suffix_huff_size >= huff_size / 10){
+                buffer[0] = 0;
+                buffer_pos = old_pos;
+            }
+            encoder.postprocess_encode();
+
+        }
         //timer.stop("huff");
        // timer.start();
         auto cmpSize = lossless.compress(buffer, buffer_pos - buffer, cmpData, cmpCap);
       //  timer.stop("zstd");
       //  timer.start();
         free(buffer);
+        
         //std::cout<<"compress ended."<<std::endl; 
 
         return cmpSize;
@@ -83,10 +111,25 @@ class SZGenericCompressor : public concepts::CompressorInterface<T> {
         decomposition.load(bufferPos, bufferSize);
         encoder.load(bufferPos, bufferSize);
 
+        bool have_suffix = *buffer_pos;
+        buffer_pos++;
+
         size_t quant_inds_size = 0;
         read(quant_inds_size, bufferPos);
         auto quant_inds = encoder.decode(bufferPos, quant_inds_size);
         encoder.postprocess_decode();
+
+        if(have_suffix){
+            size_t suffix_quant_inds_size = 0;
+            read(suffix_quant_inds_size, bufferPos);
+            auto suffix_quant_inds = encoder.decode(bufferPos, quant_inds_size);
+            encoder.postprocess_decode();
+            quant_inds.resize(quant_inds.size()+suffix_quant_inds.size());
+            quant_inds.insert(quant_inds.end(),suffix_quant_inds.begin(),suffix_quant_inds,end())
+            suffix_quant_inds.clear();
+
+        }
+
 
         free(buffer);
 
