3beaf370992c0259badf146fda9d89446090b6b4
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 02:14:48 -0600

    omp

--- tools/sz3/sz3.cpp
+++ tools/sz3/sz3.cpp
@@ -473,15 +473,16 @@ int main(int argc, char *argv[]) {
         }
 
         if (dataType == SZ_FLOAT) {
+
             decompress<float>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
-#if (!SZ3_DEBUG_TIMINGS)
+/*#if (!SZ3_DEBUG_TIMINGS)
         } else if (dataType == SZ_DOUBLE) {
             decompress<double>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
         } else if (dataType == SZ_INT32) {
             decompress<int32_t>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
         } else if (dataType == SZ_INT64) {
             decompress<int64_t>(inPath, cmpPath, decPath, conf, binaryOutput, printCmpResults);
-#endif
+#endif*/
         } else {
             printf("Error: data type not supported \n");
             usage();
