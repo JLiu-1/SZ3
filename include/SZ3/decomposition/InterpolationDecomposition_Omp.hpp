1b73931682fa2e8829255ec59dda63002a7982e6
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 15:32:07 -0600

    q reorder

--- include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
@@ -306,7 +306,7 @@ class InterpolationDecomposition_OMP : public concepts::DecompositionInterface<T
                     reordered_idx += level_prefix[level] - ((x + 1) >> 1) * reduced_dim_offsets[level + 1][0] - (x % 2 == 0) * ((y + 1) >> 1) * reduced_dim_offsets[level + 1][1] - (z % 2 == 0 && y % 2 == 0) * ((x + 1) >> 1);
                 }
                 if(idx >= conf.num || reordered_idx >=conf.num)
-                    std::cout<<idx<<" "<<reordered_idx<<std::endl;
+                    std::cout<<idx<<" "<<reordered_idx<<" "<<level<<std::endl;
                  assert( idx < quant_inds_vec.size() && reordered_idx < quant_inds_vec_reordered.size());
                 quant_inds_vec_reordered [reordered_idx] = quant_inds_vec[idx];
                 
