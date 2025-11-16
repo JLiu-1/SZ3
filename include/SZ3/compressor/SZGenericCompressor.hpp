185edc1f480801ed9feb187f032ccb01bfca8893
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 12:34:53 -0600

    blocked huff

--- include/SZ3/compressor/SZGenericCompressor.hpp
+++ include/SZ3/compressor/SZGenericCompressor.hpp
@@ -191,7 +191,8 @@ class SZGenericCompressor : public concepts::CompressorInterface<T> {
                 auto cur_quant_inds = encoder.decode(temp_buffer_pos, cur_len);
                  #pragma omp critical
                 std::cout<<"tid: "<<", ended at: "<<temp_buffer_pos - bufferPos - block_byte_offset<<" ,"<<cur_quant_inds.size()<<" bins extracted."<<std::endl;
-                std::move(cur_quant_inds.begin(), cur_quant_inds.end(), quant_inds.begin() + start_idx);
+                std::copy(cur_quant_inds.begin(), cur_quant_inds.end(), quant_inds.begin() + start_idx);
+                cur_quant_inds.clear();
 
 
             }
