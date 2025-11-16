3170cf05f6d4f8bf22ed028958c236306b7d941f
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 15:29:15 -0600

    q reorder

--- include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
@@ -302,7 +302,8 @@ class InterpolationDecomposition_OMP : public concepts::DecompositionInterface<T
                     z = z >> 1;
                     level++;
                 }
-                assert(level < interp_level);
+                if(level >= interp_level)
+                    std::cout<<"post: "<<x<<" "<<y<<" "<<z<<std::endl;
                 auto reordered_idx = x * reduced_dim_offsets[level][0] + y * reduced_dim_offsets[level][1] + z ;
                 if(level  < interp_level - 1){//non-anchor or not last level
                     reordered_idx += level_prefix[level] - ((x + 1) >> 1) * reduced_dim_offsets[level + 1][0] - (x % 2 == 0) * ((y + 1) >> 1) * reduced_dim_offsets[level + 1][1] - (z % 2 == 0 && y % 2 == 0) * ((x + 1) >> 1);
