542701a42c87a73b5a9643f478dd3fd820029f93
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 02:29:31 -0600

    omp

--- include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition_Omp.hpp
@@ -99,7 +99,7 @@ class InterpolationDecomposition_OMP : public concepts::DecompositionInterface<T
                 }
                 interpolation(
                     dec_data, block.get_global_index(), end_idx, interpolators[interp_id],
-                    [&](size_t idx, T &d, T pred) { d += quantizer.recover(pred, quant_inds[idx]);},// no need to use idx. the outliers will be unpacked separately (todo).
+                    [&](size_t idx, T &d, T pred) { const size_t cidx = 20679172;if(num_elements > cidx && idx == cidx) std::cout<<d<<" "<<pred<<" "<<quant_inds[idx];d += quantizer.recover(pred, quant_inds[idx]);},// no need to use idx. the outliers will be unpacked separately (todo).
                     direction_sequence_id, stride);
             }
         }
@@ -203,6 +203,7 @@ class InterpolationDecomposition_OMP : public concepts::DecompositionInterface<T
                 interpolation(
                     data, block.get_global_index(), end_idx, interpolators[interp_id],
                     [&](size_t idx, T &d, T pred) {
+                        const size_t cidx = 20679172;if(num_elements > cidx && idx == cidx) std::cout<<d<<" "<<pred<<" "<<quant_inds[idx];
                         quant_inds[idx] = (quantizer.quantize_and_overwrite(d, pred, idx));
                     },
                     direction_sequence_id, stride);
