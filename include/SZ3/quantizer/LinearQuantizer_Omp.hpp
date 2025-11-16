e43c3c7eacba01aa5c1d9e5468e99fbcdf823e71
Jliu-1 <jliu217@central.uh.edu>
2025-11-16 03:14:13 -0600

    omp

--- include/SZ3/quantizer/LinearQuantizer_Omp.hpp
+++ include/SZ3/quantizer/LinearQuantizer_Omp.hpp
@@ -44,7 +44,8 @@ class LinearQuantizerOMP : public concepts::QuantizerOMPInterface<T, int> {
         T diff = data - pred;
         int quant_index = std::llrint(std::abs(diff) * this->double_error_bound_reciprocal);
         if (quant_index < this->radius ) {
-            
+            if (diff < 0) 
+                quant_index = -quant_index;
             T decompressed_data = pred + quant_index * this->double_error_bound;
 
             if(data_idx == 22448644){
@@ -60,11 +61,10 @@ class LinearQuantizerOMP : public concepts::QuantizerOMPInterface<T, int> {
             // if data is NaN, the error is NaN, and NaN <= error_bound is false
             if (fabs(decompressed_data - data) <= this->error_bound) {
                 data = decompressed_data;
-                if (diff < 0) 
-                    quant_index = -quant_index;
-                auto quant_index_shifted = this->radius + quant_index;
                 
-                return quant_index_shifted;
+               // auto quant_index_shifted = ;
+                
+                return this->radius + quant_index;
             } else {
                 if(data_idx == 22448644)
               std::cout<<"bb"<<std::endl;
