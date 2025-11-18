92f57c16c5d8af91bbb8d7b4c2ec16b40ca1d92e
Jliu-1 <jliu217@central.uh.edu>
2025-11-17 23:13:04 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -495,7 +495,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
     double eb_alpha = -1;
     double eb_beta = -1;
     double eb_ratio = 0.5;  // To be deprecated
-    double time = 0;
+    size_t time = 0;
     //Timer quant_timer = Timer(false);
 };
 
