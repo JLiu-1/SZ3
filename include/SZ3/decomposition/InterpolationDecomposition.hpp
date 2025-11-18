c5172cb5e90fdbfa001dadcb08d1f42f050d82a8
Jliu-1 <jliu217@central.uh.edu>
2025-11-17 22:50:10 -0600

    upd

--- include/SZ3/decomposition/InterpolationDecomposition.hpp
+++ include/SZ3/decomposition/InterpolationDecomposition.hpp
@@ -486,7 +486,7 @@ class InterpolationDecomposition : public concepts::DecompositionInterface<T, in
     double eb_beta = -1;
     double eb_ratio = 0.5;  // To be deprecated
     double time = 0;
-    Timer quant_timer = Timer(true);
+    Timer quant_timer = Timer(false);
 };
 
 template <class T, uint N, class Quantizer>
