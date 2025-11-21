e39a5adc7248661883514b7eb96303d8f3324ef5
Jliu-1 <jliu217@central.uh.edu>
2025-11-21 14:43:29 -0600

    newq

--- include/SZ3/quantizer/LinearQuantizer.hpp
+++ include/SZ3/quantizer/LinearQuantizer.hpp
@@ -35,14 +35,15 @@ class LinearQuantizer : public concepts::QuantizerInterface<T, int> {
     // int quantize(T data, T pred, T& dec_data);
     ALWAYS_INLINE int quantize_and_overwrite(T &data, T pred) override {
         T diff = data - pred;
-        int quant_index = std::llrint(diff * this->double_error_bound_reciprocal);
-        if (std::abs(quant_index) < this->radius ) {
+        int quant_index = static_cast<int>(std::nearbyint(diff * this->double_error_bound_reciprocal));
+        if (quant_index > -this->radius && quant_index < this->radius ) {
             //if (diff < 0) 
             //    quant_index = -quant_index;
             //auto quant_index_shifted = this->radius + quant_index;
             T decompressed_data = pred + quant_index * this->double_error_bound;
             // if data is NaN, the error is NaN, and NaN <= error_bound is false
-            if (fabs(decompressed_data - data) <= this->error_bound) {
+            T err = decompressed_data - data;
+            if (err >= -this->error_bound && err <= this->error_bound) {
                 data = decompressed_data;
                 return this->radius + quant_index;
             } else {
