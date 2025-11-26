12b36b66cb276eafdffb0c1fda9f721db0b3beda
Jliu-1 <jliu217@central.uh.edu>
2025-11-25 23:18:31 -0600

    debug

--- include/SZ3/sperr/SPECK_FLT.h
+++ include/SZ3/sperr/SPECK_FLT.h
@@ -639,6 +639,8 @@ auto sperr::SPECK_FLT::compress() -> RTNType
         for (auto out : recovered)
           m_vals_d[out.pos] += out.err;
 
+        m_conditioner.inverse_condition(m_vals_d, m_dims, m_condi_bitstream)
+
         //double maxdiff = 0.0;
         //for(size_t i=0;i<m_vals_orig.size();i++){
         //  maxdiff = std::max(maxdiff,std::abs(m_vals_orig[i]-m_vals_d[i]));
