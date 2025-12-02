5f312506f4789be09c94a1d680cc8723d8fe6b00
Jliu-1 <jliu217@central.uh.edu>
2025-12-01 21:49:38 -0600

    bugfix

--- include/SZ3/encoder/HuffmanEncoder.hpp
+++ include/SZ3/encoder/HuffmanEncoder.hpp
@@ -75,12 +75,11 @@ class HuffmanEncoder : public concepts::EncoderInterface<T> {
         huffmanTree->code = static_cast<uint64_t *>(malloc(huffmanTree->stateNum * sizeof(uint64_t)));
         huffmanTree->cout = static_cast<unsigned char *>(malloc(huffmanTree->stateNum * sizeof(unsigned char)));
 
-memset(huffmanTree->code, 0, huffmanTree->stateNum * sizeof(uint64_t));
-memset(huffmanTree->cout, 0, huffmanTree->stateNum * sizeof(unsigned char));
+
 
         memset(huffmanTree->pool, 0, huffmanTree->allNodes * 2 * sizeof(struct node_t));
         memset(huffmanTree->qqq, 0, huffmanTree->allNodes * 2 * sizeof(node));
-        memset(huffmanTree->code, 0, huffmanTree->stateNum * sizeof(uint64_t *));
+        memset(huffmanTree->code, 0, huffmanTree->stateNum * sizeof(uint64_t));
         memset(huffmanTree->cout, 0, huffmanTree->stateNum * sizeof(unsigned char));
         huffmanTree->qq = huffmanTree->qqq - 1;
         huffmanTree->n_nodes = 0;
